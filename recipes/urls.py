a801b08a17ff1a86c1d4fc2d33f77454b98cba59
AllanCorte <allanalvescorte@gmail.com>
2026-03-12 20:22:44 -0400

    Criando a header, separando um partial para head e usando include no template

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from recipes.views import home, contato
-
+from recipes.views import home
 urlpatterns = [
     path('', home),  # home
-    path('contato/', contato),  # contato
 ]
